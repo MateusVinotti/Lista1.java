841d0370de7d512d692a7094cd3da4043bf61bf3
Vinotti <mateusvinotti@gmail.com>
2024-03-15 20:12:02 -0300

    Exercicio6

---
+++ Exercicio6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = (9 * celsius + 160) / 5;
+
+        System.out.println("Temperatura em Fahrenheit: " + fahrenheit);
+
+        scanner.close();
+    }
+
+}
